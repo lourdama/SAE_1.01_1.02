5d67196e3fd35a9aaf247921b7e95a047f8e1375
Raphael Coltelli <Raphael.Coltelli@etu.univ-smb.fr>
2024-12-10 08:20:18 +0100

    Deplacement et InnitTimer

--- JeuxPlateformeBille/MainWindow.xaml.cs
+++ JeuxPlateformeBille/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace JeuxPlateformeBille
 {
@@ -16,9 +19,128 @@ namespace JeuxPlateformeBille
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer minuterie;
+        private bool gauche, droite, saut = false, enSaut;
+        private int vitesseJoueur = 8, sautJoueur = 8, gravite = 3;
+        System.Drawing.Rectangle hitBoxSol, hitBoxJoueur;
         public MainWindow()
         {
+
+            
             InitializeComponent();
+            InitTimer();
+            hitBoxSol = new System.Drawing.Rectangle((int)Canvas.GetLeft(sol), (int)Canvas.GetTop(sol), (int)sol.Width, (int)sol.Height);
+            this.KeyDown += new KeyEventHandler(Window_KeyDown);
+            this.KeyUp += new KeyEventHandler(Window_KeyUp);
+            
         }
+
+        private void InitTimer()
+        {
+            minuterie = new DispatcherTimer();
+            minuterie.Interval = TimeSpan.FromMilliseconds(17);
+            minuterie.Tick += Jeu;
+            minuterie.Start();
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                gauche = true;
+            }
+
+            else if (e.Key == Key.Right)
+            {
+                droite = true;
+            }
+
+            else if (e.Key == Key.Space)
+            {
+                saut = true;
+            }
+        }
+
+        
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                gauche = false;
+            }
+            else if (e.Key == Key.Right) 
+            {
+                droite = false;
+            }
+                
+            else if (e.Key == Key.Space)
+            {
+                saut = false;
+            }
+        }
+        private void Jeu(object? sender, EventArgs e)
+        {
+            deplacement();
+
+        }
+        private void deplacement()
+        {
+
+            hitBoxJoueur = new System.Drawing.Rectangle((int)Canvas.GetLeft(joueur), (int)Canvas.GetTop(joueur), (int)joueur.Width, (int)joueur.Height);
+            if (hitBoxJoueur.IntersectsWith(hitBoxSol))
+            {
+                gravite = 0;
+            }
+            Canvas.SetTop(joueur, Canvas.GetTop(joueur) + gravite);
+            
+            if (droite)
+            {
+                if ((Canvas.GetLeft(joueur) + sautJoueur) + joueur.Width < this.ActualWidth)
+                {
+                    Canvas.SetLeft(joueur, Canvas.GetLeft(joueur) + sautJoueur);
+                }
+                
+            }
+
+            if (gauche)
+            {
+                if ((Canvas.GetLeft(joueur) - sautJoueur) > 0)
+                {
+                    Canvas.SetLeft(joueur, Canvas.GetLeft(joueur) - sautJoueur);
+                }
+                    
+            }
+
+
+            if (saut && auSol())
+            {
+                enSaut = true;
+                Canvas.SetTop(joueur, Canvas.GetTop(joueur) - sautJoueur);
+            }
+
+            /*if (!auSol())
+            {
+                gravite = 3;
+            }
+            else
+            {
+                gravite = 0;
+                if (Canvas.GetTop(joueur) > hitBoxSol.Top - joueur.Height)
+                {
+                    Canvas.SetTop(joueur, hitBoxSol.Top - joueur.Height);
+                    enSaut = false; 
+                }
+
+            }*/
+        }
+        private bool auSol()
+        {
+
+            hitBoxJoueur = new System.Drawing.Rectangle((int)Canvas.GetLeft(joueur), (int)Canvas.GetTop(joueur), (int)joueur.Width, (int)joueur.Height);
+            bool estAuSol = hitBoxSol.IntersectsWith(hitBoxJoueur);
+            return estAuSol;
+        }
+        
+
     }
 }
