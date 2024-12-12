e283ad382faa46c42c8910feb29ff7d2f2cb1663
Mattys Lourdault <Mattys.Lourdault@etu.univ-smb.fr>
2024-12-12 11:39:15 +0100

    Restrucuration complète de la gestion des liste et des paramètres des ennemis. Liste + classe

--- JeuxPlateformeBille/MainWindow.xaml.cs
+++ JeuxPlateformeBille/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -30,12 +31,15 @@ namespace JeuxPlateformeBille
         System.Drawing.Rectangle hitBoxSol, hitBoxJoueur,hitBoxBille, hitBoxEnnemi;
         private static Point clickPosition;
         private static double vitesseSaut, graviteBille = 4, coefReductionDeplacementSaut;
-        private static List<Image> billesEnJeu = billesEnJeu = new List<Image>();
+      /*  private static List<Image> billesEnJeu = billesEnJeu = new List<Image>();
         private static List<double[]> vitesseBilles = new List<double[]>();
         private static List<Image> ennemisEnJeu = ennemisEnJeu = new List<Image>();
         private static List<double[]> vitesseEnnemis = new List<double[]>();
-        private static List<double[]> InitVarEnnemis = new List<double[]>();
+        private static List<double[]> InitVarEnnemis = new List<double[]>();*/
         private static Ennemis fantome = new Ennemis();
+        private static Billes billeClassique = new Billes();
+        private static List<Ennemis> ennemisEnJeu = new List<Ennemis>();
+        private static List<Billes> billesEnJeu = new List<Billes>();
         private static Random aleatoire = new Random();
 
         public MainWindow()
@@ -57,7 +61,7 @@ namespace JeuxPlateformeBille
             InitJeu();
             InitTimer();
             InitEnnemis();
-            spawnEnnemi();
+            //spawnEnnemi();
         }
 
         private void InitJeu()
@@ -79,15 +83,18 @@ namespace JeuxPlateformeBille
 
         private void InitEnnemis()
         {
-            ennemisEnJeu.Insert(0, new Image());
-            ennemisEnJeu[0].Source = imgBille;
-            ennemisEnJeu[0].Width = 30;
-            ennemisEnJeu[0].Height = 50;
-            canvasMainWindow.Children.Add(ennemisEnJeu[0]);
-            Canvas.SetTop(ennemisEnJeu[0], aleatoire.Next(0, 900));
-            Canvas.SetLeft(ennemisEnJeu[0], aleatoire.Next(0, 1500));
-            vitesseEnnemis.Insert(0, new double[2]);
-            vitesseEnnemis[0] = [2];
+            // Initialisation ennemi classique
+            fantome.Texture = new Image();
+            fantome.Texture.Source = imgBille;
+            fantome.Texture.Width = 50;
+            fantome.Texture.Height = 30;
+            fantome.CoordonneeX = 500;
+            fantome.CoordonneeY = 500;
+            fantome.Vitesse = 2;
+            ennemisEnJeu.Insert(0, fantome);
+            canvasMainWindow.Children.Add(ennemisEnJeu[0].Texture);
+            Canvas.SetTop(ennemisEnJeu[0].Texture, ennemisEnJeu[0].CoordonneeY);
+            Canvas.SetLeft(ennemisEnJeu[0].Texture, ennemisEnJeu[0].CoordonneeX);
         }
         private void InitTimer()
         {
@@ -157,9 +164,9 @@ namespace JeuxPlateformeBille
 
             for (int i = 0; i < billesEnJeu.Count; i++)
             {
-                if (billeLance(billesEnJeu[i], vitesseBilles[i]))
+                if (billeLance(billesEnJeu[i].Texture, billesEnJeu[i].Vitesse))
                 {
-                    canvasMainWindow.Children.Remove(billesEnJeu[i]);
+                    canvasMainWindow.Children.Remove(billesEnJeu[i].Texture);
                     billesEnJeu.RemoveAt(i);
                 }
             }
@@ -243,16 +250,16 @@ namespace JeuxPlateformeBille
         {
             if (nbStockBille > 0)
             {
+                billeClassique.Texture = new Image();
+                billeClassique.Texture.Source = imgBille;
+                billeClassique.Texture.Width = 15;
+                billeClassique.Texture.Height = 15;
                 clickPosition = e.GetPosition(this);
-                billesEnJeu.Insert(0, new Image());
-                billesEnJeu[0].Source = imgBille;
-                billesEnJeu[0].Width = 15;
-                billesEnJeu[0].Height = 15;
-                canvasMainWindow.Children.Add(billesEnJeu[0]);
-                Canvas.SetTop(billesEnJeu[0], Canvas.GetTop(joueur));
-                Canvas.SetLeft(billesEnJeu[0], Canvas.GetLeft(joueur));
-                vitesseBilles.Insert(0, new double[2]);
-                vitesseBilles[0] = [clickPosition.X - Canvas.GetLeft(billesEnJeu[0]), clickPosition.Y - Canvas.GetTop(billesEnJeu[0])];
+                billesEnJeu.Insert(0, billeClassique);
+                canvasMainWindow.Children.Add(billesEnJeu[0].Texture);
+                Canvas.SetTop(billesEnJeu[0].Texture, Canvas.GetTop(joueur));
+                Canvas.SetLeft(billesEnJeu[0].Texture, Canvas.GetLeft(joueur));
+                billesEnJeu[0].Vitesse = [clickPosition.X - Canvas.GetLeft(billesEnJeu[0].Texture), clickPosition.Y - Canvas.GetTop(billesEnJeu[0].Texture)];
                 billeBouge = true;
                 nbStockBille = nbStockBille - 1;
                 StockBille.Content = "Stock De Billes : " + nbStockBille;
@@ -286,7 +293,7 @@ namespace JeuxPlateformeBille
             
             
         }
-        private void spawnEnnemi()
+      /*  private void spawnEnnemi()
         {
             ennemisEnJeu.Insert(0, new Image());
             ennemisEnJeu[0].Source = imgBille;
@@ -297,13 +304,13 @@ namespace JeuxPlateformeBille
             Canvas.SetLeft(ennemisEnJeu[0], aleatoire.Next(0, 1500));
             vitesseEnnemis.Insert(0, new double[2]);
             vitesseEnnemis[0] = [2];
-        }
+        }*/
 
         private void deplacementEnnemi()
         {
-            Canvas.SetLeft(ennemisEnJeu[0], Canvas.GetLeft(ennemisEnJeu[0]) + Math.Sign(Canvas.GetLeft(joueur) - Canvas.GetLeft(ennemisEnJeu[0])) * 2);
-            Canvas.SetTop(ennemisEnJeu[0], Canvas.GetTop(ennemisEnJeu[0]) + Math.Sign(Canvas.GetTop(joueur) - Canvas.GetTop(ennemisEnJeu[0])) );
-            hitBoxEnnemi = new System.Drawing.Rectangle((int)Canvas.GetLeft(ennemisEnJeu[0]), (int)Canvas.GetTop(ennemisEnJeu[0]), (int)ennemisEnJeu[0].Width, (int)ennemisEnJeu[0].Height);
+            Canvas.SetLeft(ennemisEnJeu[0].Texture, Canvas.GetLeft(ennemisEnJeu[0].Texture) + Math.Sign(Canvas.GetLeft(joueur) - Canvas.GetLeft(ennemisEnJeu[0].Texture)) * 2);
+            Canvas.SetTop(ennemisEnJeu[0].Texture, Canvas.GetTop(ennemisEnJeu[0].Texture) + Math.Sign(Canvas.GetTop(joueur) - Canvas.GetTop(ennemisEnJeu[0].Texture)) );
+            hitBoxEnnemi = new System.Drawing.Rectangle((int)Canvas.GetLeft(ennemisEnJeu[0].Texture), (int)Canvas.GetTop(ennemisEnJeu[0].Texture), (int)ennemisEnJeu[0].Texture.Width, (int)ennemisEnJeu[0].Texture.Height);
            /* Canvas.SetLeft(EnnemiVie, Canvas.GetLeft(ennemi) + 15);
             Canvas.SetLeft(EnnemiVie2, Canvas.GetLeft(ennemi) + 30);
             Canvas.SetTop(EnnemiVie, Canvas.GetTop(ennemi) - 10);
@@ -314,11 +321,11 @@ namespace JeuxPlateformeBille
         {
             if (hitBoxBille.IntersectsWith(hitBoxEnnemi) )
             {
-                billesEnJeu[0].Visibility = Visibility.Hidden;
-                Canvas.SetLeft(billesEnJeu[0], -10);
+                billesEnJeu[0].Texture.Visibility = Visibility.Hidden;
+                Canvas.SetLeft(billesEnJeu[0].Texture, -10);
                 if (nbtouche == 1)
                 {
-                    ennemisEnJeu[0].Visibility = Visibility.Hidden;
+                    ennemisEnJeu[0].Texture.Visibility = Visibility.Hidden;
                     //EnnemiVie.Visibility = Visibility.Hidden;
                     //EnnemiVie2.Visibility = Visibility.Hidden;
                     ReinitialisationSaut();
@@ -336,10 +343,10 @@ namespace JeuxPlateformeBille
         }
         private bool VerifTouche()
         {
-            if (ennemisEnJeu[0].Visibility == Visibility.Visible)
+            if (ennemisEnJeu[0].Texture.Visibility == Visibility.Visible)
             {
                 hitBoxJoueur = new System.Drawing.Rectangle((int)Canvas.GetLeft(joueur), (int)Canvas.GetTop(joueur), (int)joueur.Width - 2, (int)joueur.Height - 2);
-                hitBoxEnnemi = new System.Drawing.Rectangle((int)Canvas.GetLeft(ennemisEnJeu[0]), (int)Canvas.GetTop(ennemisEnJeu[0]), (int)ennemisEnJeu[0].Width - 2, (int)ennemisEnJeu[0].Height - 2);
+                hitBoxEnnemi = new System.Drawing.Rectangle((int)Canvas.GetLeft(ennemisEnJeu[0].Texture), (int)Canvas.GetTop(ennemisEnJeu[0].Texture), (int)ennemisEnJeu[0].Texture.Width - 2, (int)ennemisEnJeu[0].Texture.Height - 2);
                 bool ennemiTouche = hitBoxEnnemi.IntersectsWith(hitBoxJoueur);
                 return ennemiTouche;
             }
@@ -359,7 +366,7 @@ namespace JeuxPlateformeBille
     }
     public partial class Ennemis
     {
-       private int coordonneeX, coordonneeY, largeur, longueur, typeDeplacement;
+       private int coordonneeX, coordonneeY, typeDeplacement;
        private Image texture;
        private double vitesse;
 
@@ -378,16 +385,6 @@ namespace JeuxPlateformeBille
             get { return coordonneeY; }
             set { coordonneeY = value; }
         }
-        public int Largeur
-        {
-            get { return largeur; }
-            set { largeur = value; }
-        }
-        public int Longueur
-        {
-            get { return longueur; }
-            set { longueur = value; }
-        }
         public int TypeDeplacement
         {
             get { return typeDeplacement; }
@@ -401,31 +398,21 @@ namespace JeuxPlateformeBille
     }
     public partial class Billes
     {
-        private int largeur, longueur, typeBille;
+        private int typeBille;
         private Image texture;
-        private double vitesse;
+        private double[] vitesse;
 
         public Image Texture
         {
             get { return texture; }
             set { texture = value; }
         }
-        public int Largeur
-        {
-            get { return largeur; }
-            set { largeur = value; }
-        }
-        public int Longueur
-        {
-            get { return longueur; }
-            set { longueur = value; }
-        }
         public int TypeBille
         {
             get { return typeBille; }
             set { typeBille = value; }
         }
-        public double Vitesse
+        public double[] Vitesse
         {
             get { return vitesse; }
             set { vitesse = value; }
