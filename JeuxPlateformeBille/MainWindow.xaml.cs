184f53c8d5e2c54478ba22cb920d8d6fdb2927b8
Mattys Lourdault <Mattys.Lourdault@etu.univ-smb.fr>
2024-12-16 13:30:09 +0100

    Plateforme coordonnée

--- JeuxPlateformeBille/MainWindow.xaml.cs
+++ JeuxPlateformeBille/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace JeuxPlateformeBille
         public DispatcherTimer minuterie;
         private static BitmapImage imgBille, fond;
         private bool gauche, droite, saut, enSaut, billeBouge, pause = false;
-        private int vitesseJoueur = 8, gravite = 8, toleranceColision = 8, nbtouche = 0, nbStockBille = 1000, niveau = 0, choixBille;
+        private int vitesseJoueur = 8, gravite = 8, toleranceColision = 12, nbtouche = 0, nbStockBille = 1000, niveau = 0, choixBille;
         System.Drawing.Rectangle hitBoxSol, hitBoxJoueur, hitBoxBille, hitBoxEnnemi;
         private static Point clickPosition;
         private static double vitesseSaut, graviteBille = 4, coefReductionDeplacementSaut;
@@ -39,8 +39,10 @@ namespace JeuxPlateformeBille
 
         int[][,] coordonneesPlateformes = new int[][,]
         {
-            new int[,] { {50, 500,7500 }, {300, 600, 600 } },
-           
+         new int[,] { {50, 500,7500 }, {300, 600, 600 } },
+         new int[,] { { },{ } },
+         new int[,] { { },{ } },
+         new int[,] { { },{ } },
         };
         private static Random aleatoire = new Random();
 
@@ -365,7 +367,6 @@ namespace JeuxPlateformeBille
                 colisionEnnemi();
             }
             hitBoxBille = new System.Drawing.Rectangle((int)Canvas.GetLeft(bille.Texture), (int)Canvas.GetTop(bille.Texture), (int)bille.Texture.Width - 1, (int)bille.Texture.Height - 1);
-           
             for (int i = 0; i < plateformesEnJeu.Count; i++)
             {
                 if (plateformesEnJeu[i].BoiteCollision.IntersectsWith(hitBoxBille))
