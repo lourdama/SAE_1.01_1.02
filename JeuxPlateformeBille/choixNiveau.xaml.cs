7841322f527fbf5a7da4f41cfbccd36c2ea7b965
Raphael Coltelli <Raphael.Coltelli@etu.univ-smb.fr>
2024-12-17 18:06:01 +0100

    .

--- JeuxPlateformeBille/choixNiveau.xaml.cs
+++ JeuxPlateformeBille/choixNiveau.xaml.cs
@@ -27,8 +27,74 @@ namespace JeuxPlateformeBille
 
         private void ellipseNiveau1_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).Suivant();
-            ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).ControlContent.Content = null;
+            if (((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau == 0)
+            {
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau = 1;
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).Suivant();
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).ControlContent.Content = null;
+
+            }
+            
         }
+
+        private void ellipseNiveau2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau == 1)
+            {
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau = 2;
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).Suivant();
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).ControlContent.Content = null;
+
+            }
+        }
+        private void ellipseNiveau3_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau == 2)
+            {
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau = 3;
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).Suivant();
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).ControlContent.Content = null;
+
+            }
+        }
+        private void ellipseNiveau4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau == 3)
+            {
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).niveau = 4;
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).Suivant();
+                ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).ControlContent.Content = null;
+
+            }
+        }
+
+        private void retour_Click(object sender, RoutedEventArgs e)
+        {
+            ChoixNiveau choixDuNiveau = new ChoixNiveau();
+            ImageBrush imageBrush = new ImageBrush();
+            imageBrush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/img/castle.jpg", UriKind.RelativeOrAbsolute));
+            ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).canvasMainWindow.Background = imageBrush;
+            ((MainWindow)((Canvas)((ContentControl)this.Parent).Parent).Parent).ControlContent.Content = new Accueil();
+
+        }
+        public void ChangerCouleurEllipseNiveau(int niveau)
+        {
+
+
+
+            if (niveau >= 1)
+            {
+                ellipseNiveau1.Fill = Brushes.Green;
+            }
+            if (niveau >=2)
+            {
+                ellipseNiveau2.Fill = Brushes.Green;
+            }
+            if (niveau >= 3)
+            {
+                ellipseNiveau3.Fill = Brushes.Green;
+            }
+        }
+
     }
 }
